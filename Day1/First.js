49f8acda5771cbba0b2e22a4bb57ea1b1de53788
Bittu kumar <bittukumarab149@gmail.com>
2026-01-04 14:55:06 +0530

    message

--- Day1/First.js
+++ Day1/First.js
@@ -1,15 +1,33 @@
 // Create Element through js
-const header1 = document.createElement("h1");
-header1.innerText = "Hello World from js";
-header1.style.backgroundColor = "blue";
-header1.style.fontSize = "30px";
-header1.style.color = "white";
+//46 commits
+//styles = {fontsize:"30px",backgroundcolor:"blue",color:"white"}
+const React ={
+    createElement: function(tag,styles,children){
+        const element = document.createElement(tag);
 
-const header2 = document.createElement("h2");
-header2.innerText = "Hello World";
-header2.style.backgroundColor = "black";
-header2.style.fontSize = "20px";
-header2.style.color = "white";
+        element.innerText = children;
+        for(let key in styles){
+            element.style[key] = styles[key];
+        }
+        return element;
+    }
+}
+
+const header1 = React.createElement('h1',{fontSize:"30px",backgroundColor:"blue",color:"white"},"Hello coder army");
+const header2 = React.createElement('h2',{fontSize:"20px",backgroundColor:"black",color:"white"},"Hello World");
+
+
+// const header1 = document.createElement("h1");
+// header1.innerText = "Hello World from js";
+// header1.style.backgroundColor = "blue";
+// header1.style.fontSize = "30px";
+// header1.style.color = "white";
+
+// const header2 = document.createElement("h2");
+// header2.innerText = "Hello World";
+// header2.style.backgroundColor = "black";
+// header2.style.fontSize = "20px";
+// header2.style.color = "white";
 
 const root = document.getElementById("root");
 root.append(header1);
