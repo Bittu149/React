208ea8783143d709defef22c9acd3ef186f6db7d
Bittu kumar <bittukumarab149@gmail.com>
2026-01-04 13:41:20 +0530

    Day1: Updated JS file

--- Day1/First.js
+++ Day1/First.js
@@ -5,5 +5,12 @@ header1.style.backgroundColor = "blue";
 header1.style.fontSize = "30px";
 header1.style.color = "white";
 
+const header2 = document.createElement("h2");
+header2.innerText = "Hello World";
+header2.style.backgroundColor = "black";
+header2.style.fontSize = "20px";
+header2.style.color = "white";
+
 const root = document.getElementById("root");
 root.append(header1);
+root.append(header2);
